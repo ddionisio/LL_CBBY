31072bc831f0a5b3da0662adf560ebd56c07788d
David Dionisio <ddionisio@renegadeware.com>
2019-07-01 20:09:50 -0400

    initial stuff

--- Assets/Scripts/Game/GameData.cs
+++ Assets/Scripts/Game/GameData.cs
@@ -4,5 +4,65 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "gameData", menuName = "Game/Game Data", order = 0)]
 public class GameData : M8.SingletonScriptableObject<GameData> {
-    
+    public const string sceneVarPlayerName = "playerName";
+
+    public string playerName {
+        get {
+            if(string.IsNullOrEmpty(mCurPlayerName))
+                InitPlayerName();
+
+            return mCurPlayerName;
+        }
+    }
+
+    public string playerInitial {
+        get {
+            if(string.IsNullOrEmpty(mCurPlayerInitial))
+                InitPlayerName();
+
+            return mCurPlayerInitial;
+        }
+    }
+
+    private string mCurPlayerName;
+    private string mCurPlayerInitial;
+
+    public void SetPlayerName(string aPlayerName) {
+        mCurPlayerName = aPlayerName;
+
+        M8.SceneState.instance.global.SetValueString(sceneVarPlayerName, mCurPlayerName, true);
+
+        GeneratePlayerInitial();
+    }
+
+    private void InitPlayerName() {
+        mCurPlayerName = M8.SceneState.instance.global.GetValueString(sceneVarPlayerName);
+        GeneratePlayerInitial();
+    }
+
+    private void GeneratePlayerInitial() {
+        if(!string.IsNullOrEmpty(mCurPlayerName)) {
+            var words = mCurPlayerName.Split(' ');
+            if(words.Length > 1) {
+                var sb = new System.Text.StringBuilder(2);
+
+                var firstWord = words[0];
+                var lastWord = words[words.Length - 1];
+
+                if(!string.IsNullOrEmpty(firstWord))
+                    sb.Append(firstWord[0]);
+
+                if(!string.IsNullOrEmpty(lastWord))
+                    sb.Append(lastWord[0]);
+
+                mCurPlayerInitial = sb.ToString().ToUpper();
+            }
+            else if(!string.IsNullOrEmpty(words[0]))
+                mCurPlayerInitial = words[0].Substring(0, 1).ToUpper();
+            else
+                mCurPlayerInitial = "";
+        }
+        else
+            mCurPlayerInitial = "";
+    }
 }
---
+++ Assets/Scripts/UI/Widgets/PlayerNameInput.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameInput : MonoBehaviour {
+    [Header("UI")]
+    public InputField nameInput;
+    public Text initialText;
+    public Button confirmButton;
+
+    [Header("Signal Invoke")]
+    public M8.Signal signalInvokeNext;
+
+    private string mCurName;
+    private string mCurInitial;
+
+    void OnEnable() {
+        //initialize strings
+
+        //determine if confirmButton is enabled
+    }
+
+    void Awake() {
+        
+    }
+
+    void OnNameChanged(string nameVal) {
+
+    }
+
+    void OnNameSubmit(string nameVal) {
+
+    }
+
+    void OnConfirmClick() {
+
+    }
+}
