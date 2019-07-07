fb36d6efe9a4332ff82c58a2d8e1e9a5869644d8
David Dionisio <ddionisio@renegadeware.com>
2019-07-07 18:13:25 -0400

    more on-site gameflow

---
+++ Assets/Scripts/Actions/GetCameraCaptureMax.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions.Game {
+    [ActionCategory("Game")]
+    public class GetCameraCaptureMax : FsmStateAction {
+        [UIHint(UIHint.Variable)]
+        public FsmInt output;
+
+        public override void Reset() {
+            output = null;
+        }
+
+        public override void OnEnter() {
+            output.Value = GameData.instance.captureCount;
+        }
+    }
+}
---
+++ Assets/Scripts/Actions/GetCameraCapturedCount.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions.Game {
+    [ActionCategory("Game")]
+    public class GetCameraCapturedCount : FsmStateAction {
+        [UIHint(UIHint.Variable)]
+        public FsmInt output;
+
+        public override void Reset() {
+            output = null;
+        }
+
+        public override void OnEnter() {
+            int count = 0;
+
+            var captureInfs = GameData.instance.captureInfos;
+            for(int i = 0; i < captureInfs.Length; i++) {
+                var inf = captureInfs[i];
+                if(inf.texture)
+                    count++;
+            }
+
+            output.Value = count;
+
+            Finish();
+        }
+    }
+}
